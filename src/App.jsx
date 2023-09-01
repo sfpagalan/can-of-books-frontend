06bbeac6528edee47ac8fe204d2e1ed840dbe9c8
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-09-01 14:48:00 -0700

    lab-15

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,39 @@ import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
 import About from './About';
+import Profile from './Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
-          <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            />
-            <Route 
-              path="/About"
-              element={<About />}
-            />
-          </Routes>
-          <Footer />
-        </Router>
-      </>
-    )
+function App() {
+  const { isLoading, error } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<BestBooks />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </Auth0Provider>
+  );
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Navbar, NavItem } from 'react-bootstrap';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Can Of Books</Navbar.Brand>
+function Header() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand>My Can Of Books</Navbar.Brand>
+      <Nav className="mr-auto">
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
-      </Navbar>
-    )
-  }
+      </Nav>
+      <Nav>
+        {isAuthenticated ? (
+          <NavItem onClick={() => logout({ returnTo: window.location.origin })}>
+            <span className="nav-link">Logout</span>
+          </NavItem>
+        ) : (
+          <NavItem onClick={() => loginWithRedirect()}>
+            <span className="nav-link">Login</span>
+          </NavItem>
+        )}
+      </Nav>
+    </Navbar>
+  );
 }
 
 export default Header;
